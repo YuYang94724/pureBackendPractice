e5e5922ca81fe6077575214c80292c0960c11a77
A0916 <Steven_Hou@provision.com.tw>
2023-04-22 01:27:16 +0800

    build : security config

--- src/main/java/com/yuyang/purebackendpractice/member/MemberController.java
+++ src/main/java/com/yuyang/purebackendpractice/member/MemberController.java
@@ -33,19 +33,19 @@ public class MemberController {
         this.memberService = memberService;
     }
 
-    @Operation(summary = "登入")
-    @PostMapping("/members:login")
-    ResponseEntity<String> postMembersLogin(@RequestBody LoginRequestDto loginRequestDto) {
-        return memberService
-                .login(loginRequestDto.getUsername(), loginRequestDto.getPassword())
-                .map(v -> jwtTokenService.generate(v.getUsername(), 60L * 60L * 1000L, UUID.randomUUID().toString()))
-                .map(v -> ResponseEntity
-                        .status(HttpStatus.OK)
-                        .header(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", v))
-                        .body(v)
-                )
-                .orElseThrow(() -> new RuntimeException("postMembersLogin Err"));
-    }
+//    @Operation(summary = "登入")
+//    @PostMapping("/members:login")
+//    ResponseEntity<String> postMembersLogin(@RequestBody LoginRequestDto loginRequestDto) {
+//        return memberService
+//                .login(loginRequestDto.getUsername(), loginRequestDto.getPassword())
+//                .map(v -> jwtTokenService.generate(v.getUsername(), 60L * 60L * 1000L, UUID.randomUUID().toString()))
+//                .map(v -> ResponseEntity
+//                        .status(HttpStatus.OK)
+//                        .header(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", v))
+//                        .body(v)
+//                )
+//                .orElseThrow(() -> new RuntimeException("postMembersLogin Err"));
+//    }
 //
 //    @Operation(summary = "查詢用戶資料")
 //    @PreAuthorize("hasRole('USER')")
