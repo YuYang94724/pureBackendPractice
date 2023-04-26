4e4a064da67c9f23b4e19336d85b209dc7038ea3
A0916 <Steven_Hou@provision.com.tw>
2023-04-26 17:04:40 +0800

    build : WalletPO 型別修改

--- src/main/java/com/yuyang/purebackendpractice/wallet/data/po/WalletPO.java
+++ src/main/java/com/yuyang/purebackendpractice/wallet/data/po/WalletPO.java
@@ -1,5 +1,6 @@
 package com.yuyang.purebackendpractice.wallet.data.po;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.yuyang.purebackendpractice.wallet.data.enu.WalletStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,6 +15,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Builder
 @Data
@@ -27,25 +29,35 @@ public class WalletPO implements Serializable {
     private static final long serialVersionUID = 6024047340357745034L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
     Long id;
 
+    @Column
     Long memberId;
 
+    @Column
     BigDecimal balance;
 
+    @Column
     @Enumerated(value = EnumType.STRING)
     WalletStatus status;
 
+    @Column
     @CreatedBy
     String createdBy;
 
+    @JsonFormat(pattern = "yyyyMMdd")
+    @Column
     @CreatedDate
-    Long createdDate;
+    LocalDate createdDate;
 
+    @Column
     @LastModifiedBy
     String lastModifiedBy;
 
+    @JsonFormat(pattern = "yyyyMMdd")
+    @Column
     @LastModifiedDate
-    Long lastModifiedDate;
+    LocalDate lastModifiedDate;
 }
